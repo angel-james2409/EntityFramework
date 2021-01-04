ed9f1e3cebb2895632159d9699f4268694e1aef2
Angel <ngeljames2409@gmail.com>
2021-01-04 20:55:58 +0530

    final commit

---
+++ Models/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
